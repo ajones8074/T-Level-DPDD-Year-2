053d8d57a80d46d3e61cf3a0aa124ab61598b3b6
ajones8074 <jonesalfie4326@hotmail.com>
2024-09-25 18:52:46 +0100

    Add address inputs to database

--- ReactLesson3/backend/routes/users.js
+++ ReactLesson3/backend/routes/users.js
@@ -34,7 +34,12 @@ router.post('/', async function(req, res, next) {
   
   var fname = req.body.fname;
   var sname = req.body.sname;
-  var address = req.body.address;
+  var address1 = req.body.address1;
+  var address2 = req.body.address2;
+  var addressCity = req.body.addressCity;
+  var addressCounty = req.body.addressCounty;
+  var addressCountry = req.body.addressCountry;
+  var postcode = req.body.postcode;
   var email = req.body.email;
   var password = CryptoJS.SHA512(req.body.password).toString();
 
@@ -48,7 +53,12 @@ router.post('/', async function(req, res, next) {
     var result = await collection.insertOne({
       fname:fname,
       sname:sname,
-      address:address,
+      address1:address1,
+      address2:address2,
+      addressCity:addressCity,
+      addressCounty:addressCounty,
+      addressCountry:addressCountry,
+      postcode:postcode,
       email:email,
       password:password
     })
